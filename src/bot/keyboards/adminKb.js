aa0573666cbadd0440ef7cebd3559c90d5234f32
Dima <dimatolpygin82@gmail.com>
2026-03-01 22:00:56 +0900

    add admin webapp button

--- src/bot/keyboards/adminKb.js
+++ src/bot/keyboards/adminKb.js
@@ -1,8 +1,21 @@
 import { Markup } from 'telegraf';
+import { config } from '../../config/index.js';
 
-export const adminKb = () => Markup.inlineKeyboard([
-  [Markup.button.callback('📝 Тексты (bot_content)', 'admin_content')],
-  [Markup.button.callback('💰 Цены (token_prices)', 'admin_prices')],
-  [Markup.button.callback('📦 Тарифы (bot_tariffs)', 'admin_tariffs')],
-  [Markup.button.callback('🏠 В меню', 'main_menu')],
-]);
+const baseUrl = (config.WEBAPP_URL || config.APP_URL || '').replace(/\/+$/, '');
+const adminUrl = baseUrl ? `${baseUrl}/admin` : '';
+
+export const adminKb = () => {
+  const rows = [];
+  if (adminUrl) {
+    rows.push([
+      Markup.button.webApp('🌐 Открыть веб-админку', adminUrl),
+    ]);
+  }
+  rows.push(
+    [Markup.button.callback('📝 Тексты (bot_content)', 'admin_content')],
+    [Markup.button.callback('💰 Цены (token_prices)', 'admin_prices')],
+    [Markup.button.callback('📦 Тарифы (bot_tariffs)', 'admin_tariffs')],
+    [Markup.button.callback('🏠 В меню', 'main_menu')],
+  );
+  return Markup.inlineKeyboard(rows);
+};
